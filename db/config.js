026b84a4f2f2dda06fbb6080e89de4d42258891d
darab ahmad khan <aksam25@gmail.com>
2023-01-08 14:58:36 +0530

    mongodb connection change

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/e-com',()=>{
+mongoose.connect(process.env.MONGODB_URL,()=>{
     console.log("server connected to mongoDB")
 })
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 require("./db/config");
 const cors = require("cors");
